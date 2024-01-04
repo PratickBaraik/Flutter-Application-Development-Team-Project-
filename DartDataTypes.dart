6d1347e70724785528a70f631ca59b62e66951bf
PratickB111 <animeNerd.int@protonmail.com>
2024-01-04 21:36:25 +0530

    Started data types in dart

---
+++ DartDataTypes.dart
@@ -0,0 +1,18 @@
+/// This code contains the various types of dart data types
+/// Data types in dart can be categorized into two types-
+/// 1. Single Value Type
+/// 2. Multi Value Type
+///
+/// Single Value: Numbers, Strings, Booleans
+///
+/// Multi Value Type: List, Set, Map
+
+
+void main() {
+
+  // Number data type
+  int IntegerVar = 100;
+  print("IntegerVar: $IntegerVar");
+  
+}
+
